35df1e89e7084aaa173c74b201669a5879bff892
mvaradi <mark.varadi@tss-yonder.com>
2022-02-07 16:02:13 +0200

    added jasypt encription

---
+++ playground/src/main/java/jasypt/TestJasypt.java
@@ -0,0 +1,20 @@
+package jasypt;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+public class TestJasypt {
+
+    public static void main(String[] args) {
+
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        standardPBEStringEncryptor.setPassword("Demo_Pwd!2020");
+        standardPBEStringEncryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
+        String result = standardPBEStringEncryptor.encrypt("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");
+
+        System.out.println();
+        System.out.println(result);
+        System.out.println(standardPBEStringEncryptor.decrypt(result));
+    }
+}
