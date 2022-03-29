80e05f5e5c22a7a6a5a0ee313f56ad3030e9e890
mvaradi <mark.varadi@tss-yonder.com>
2022-03-29 20:31:48 +0300

    added permissions handling

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/security/TwitterQueryUserDetailsService.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/security/TwitterQueryUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.microservices.demo.elastic.query.service.security;
 
+import com.microservices.demo.elastic.query.service.business.QueryUserService;
+import com.microservices.demo.elastic.query.service.transformer.UserPermissionToUserDetailTransformer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,10 +9,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TwitterQueryUserDetailsService implements UserDetailsService {
+
+    private final QueryUserService queryUserService;
+
+    private final UserPermissionToUserDetailTransformer userPermissionToUserDetailTransformer;
+
+
+    public TwitterQueryUserDetailsService(QueryUserService queryUserService,
+                                          UserPermissionToUserDetailTransformer userPermissionToUserDetailTransformer
+    ) {
+        this.queryUserService = queryUserService;
+        this.userPermissionToUserDetailTransformer = userPermissionToUserDetailTransformer;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return TwitterQueryUser.builder()
-                .username(username)
-                .build();
+        return queryUserService.findAllPermissionsByUsername(username)
+                .map(userPermissionToUserDetailTransformer::getUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with username " + username));
     }
 }
